2f50b7ecf06a258b33eefa438d328529af4babbe
Nathaniel Wiley <nwiley86@gmail.com>
2018-01-15 16:46:37 -0500

    begin the app

--- src/js/brkmrks.js
+++ src/js/brkmrks.js
@@ -1,3 +1,53 @@
+;var brkmrks = brkmrks || (function(){
+	var hasStorage = true,data = {tags:[],links:[],index:{}};
+	try{ localStorage; } catch (err) { hasStorage = false; }
+	function save(){if(hasStorage){localStorage.setItem('brkmrksdt',JSON.stringify(data));}}
+	if(hasStorage && localStorage.getItem('brkmrksdt') === null) save();
+	var APP = {
+		add:function(url,title,tags,ts){
+			var tempMrk = {
+				ts : ts || Date.now(),
+				url : url || '',
+				title : title || '',
+				tags : tags || ''
+			};
+			if(tempMrk.url !== ''){
+				var isNew = (data.index.hasOwnProperty(tempMrk.url))? false : true;
+				var shouldSave = false;
+				if(isNew){
+					data.index[tempMrk.url] = data.links.length;
+					data.links.push(tempMrk);
+					shouldSave = true;
+				}
+				tempMrk.tags = (typeof(tempMrk.tags) === 'string')? tempMrk.tags.split(',') : [];
+				for(var i in tempMrk.tags){
+					var tempTag = tempMrk.tags[i];
+					if(data.tags.indexOf(tempTag) === -1){
+						data.tags.push(tempTag);
+						shouldSave = true;
+					}
+					if(!isNew && data.links[data.index[tempMrk.url]].tags.indexOf(tempTag) === -1){
+						data.links[data.index[tempMrk.url]].tags.push(tempTag);
+						shouldSave = true;
+					}
+				}
+				if(shouldSave) save();
+			}			
+		},
+		import:function(xmlString){
+			if(!APP.xslt){
+				// fetch XSLT
+			}
+			// Parse Google XML string
+
+		},
+		dump:function(){return data;}
+	};
+	if(hasStorage){
+		data = JSON.parse(localStorage.getItem('brkmrksdt'));
+	}
+	return APP;
+})();
 /*
 var xsltProcessor = new XSLTProcessor();
 			var xsl = '<stylesheet version="1.0" xmlns="http://www.w3.org/1999/XSL/Transform" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"><template match="/xml_api_reply/bookmarks">[<xsl:for-each select="bookmark">{"title":"<xsl:value-of select="title"/>","url":"<xsl:value-of select="url"/>","ts":"<xsl:value-of select="timestamp"/>","id":"<xsl:value-of select="id"/>","tags":[<xsl:for-each select="labels/label">"<xsl:value-of select="."/>"<xsl:if test="position() != last()">,</xsl:if></xsl:for-each>]}<xsl:if test="position() != last()">,</xsl:if></xsl:for-each>]</template></stylesheet>';
